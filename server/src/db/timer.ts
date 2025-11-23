dc1620d2403ea3d0fb7f27cb9df847d3b682d2a7
Lara <73997200+yvmee@users.noreply.github.com>
2025-11-23 09:14:11 +0100

    userid not unique

--- server/src/db/timer.ts
+++ server/src/db/timer.ts
@@ -1,7 +1,7 @@
 import { Schema, model} from 'mongoose';
 
 const timerSchema = new Schema({
-    userId : { type: String, required: true, unique: true },
+    userId : { type: String, required: true},
     flashcardId: { type: Schema.Types.ObjectId, ref: 'DBflashcard', required: true },
     time: { type: Number, required: true },
 });
